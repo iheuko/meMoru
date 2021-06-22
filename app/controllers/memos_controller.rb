8a1a762cad20bbbc1c28d9ee4ac65208e7b50f1f
iheuko <iheukosan1@gmail.com>
2021-06-22 15:16:33 +0900

    memosコントローラーにcreateアクションを記述

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,4 +5,19 @@ class MemosController < ApplicationController
   def new
     @memo = Memo.new
   end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:memo_title, :memo, :frame_type_id).merge(user_id: current_user.id)
+  end
 end
